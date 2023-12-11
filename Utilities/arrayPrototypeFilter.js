09654669b8b654f4ef27d31e70077dd8ce9cc193
Sava Stanchev <s.stanchev94@gmail.com>
2023-12-11 13:47:15 +0200

    implement array prototype filter method

---
+++ Utilities/arrayPrototypeFilter.js
@@ -0,0 +1,19 @@
+/**
+ * @template T
+ * @param { (value: T, index: number, array: Array<T>) => boolean } callbackFn
+ * @param {any} [thisArg]
+ * @return {Array<T>}
+ */
+Array.prototype.myFilter = function (callbackFn, thisArg) {
+  const len = this.length;
+  const results = [];
+
+  for (let k = 0; k < len; k++) {
+    const kValue = this[k];
+    if (Object.hasOwn(this, k) && callbackFn.call(thisArg, kValue, k, this)) {
+      results.push(kValue);
+    }
+  }
+
+  return results;
+};
